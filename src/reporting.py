8818b7806c0addfb4c7f6a1f6403156d06915e1d
DC104 <debosmitachakraborty11@gmail.com>
2026-01-27 13:23:59 +0530

    Updated

--- src/reporting.py
+++ src/reporting.py
@@ -8,7 +8,8 @@ from fpdf import FPDF
 class ReportGenerator:
     def __init__(self, output_dir="customer_auditor/data/reports"):
         self.output_dir = output_dir
-        self.sender_email = "<Your Email>"
+        self.sender_email = "debosmita812@gmail.com"
+        self.sender_pass = "myjn fwkq lyjo gopn"
 
     def generate_pdf(self, audit_data, filename="audit_report.pdf"):
         pdf = FPDF()
